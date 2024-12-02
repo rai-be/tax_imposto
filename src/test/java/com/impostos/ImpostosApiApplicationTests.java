d1209ea8c7ceaa1ba5146f4296a8a9d23ae72685
raiani <raiani.guimaraes@zup.com.br>
2024-12-02 12:14:34 -0300

    feat: add ClassApplicationTest

---
+++ src/test/java/com/impostos/ImpostosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
