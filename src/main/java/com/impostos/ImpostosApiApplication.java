a9cf45fc9efc4f1be23a7a2eb9209a58e79b0683
raiani <raiani.guimaraes@zup.com.br>
2024-12-02 12:12:26 -0300

    feat: add ClassApplication

---
+++ src/main/java/com/impostos/ImpostosApiApplication.java
@@ -0,0 +1,13 @@
+package com.impostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostosApiApplication.class, args);
+	}
+
+}
